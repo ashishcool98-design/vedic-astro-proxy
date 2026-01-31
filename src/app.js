ff41eb66d0de0a0aea343edea731f4536381295c
ashishcool98-design <ashishcool98@gmail.com>
2026-02-01 02:25:47 +0530

    Fix default export in app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+import express from "express";
+import cors from "cors";
+import astroRoutes from "./routes/astro.routes.js";
+import errorHandler from "./middleware/error.middleware.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 app.use("/astro", astroRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ status: "Vedic Astrology Proxy API running" });
+});
+
+app.use(errorHandler);
+
+export default app;
